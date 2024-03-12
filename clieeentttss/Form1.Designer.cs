1b4cb759d9afe821654172379fff4138a426d699
Legenda <smer4ak@mail.ru>
2024-03-12 19:32:49 +0500

    Добавьте файлы проекта.

---
+++ clieeentttss/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace clieeentttss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxChat = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.btnsend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxChat
+            // 
+            this.textBoxChat.Location = new System.Drawing.Point(-2, 237);
+            this.textBoxChat.Multiline = true;
+            this.textBoxChat.Name = "textBoxChat";
+            this.textBoxChat.Size = new System.Drawing.Size(321, 212);
+            this.textBoxChat.TabIndex = 0;
+            this.textBoxChat.TextChanged += new System.EventHandler(this.textBoxChat_TextChanged);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(532, 237);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(268, 212);
+            this.textBoxMessage.TabIndex = 1;
+            this.textBoxMessage.TextChanged += new System.EventHandler(this.textBoxMessage_TextChanged);
+            // 
+            // btnsend
+            // 
+            this.btnsend.Location = new System.Drawing.Point(577, 150);
+            this.btnsend.Name = "btnsend";
+            this.btnsend.Size = new System.Drawing.Size(118, 61);
+            this.btnsend.TabIndex = 2;
+            this.btnsend.Text = "Отправка";
+            this.btnsend.UseVisualStyleBackColor = true;
+            this.btnsend.Click += new System.EventHandler(this.btnsend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.textBoxChat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxChat;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button btnsend;
+    }
+}
+
---
+++ clieeentttss/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+
+
+namespace clieeentttss
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new TcpClient();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string serverIP = "";
+
+            try
+            {
+                client = new TcpClient();
+                client.Connect(serverIP, 8888);
+
+                if (client.Connected)
+                {
+                    MessageBox.Show("Ты был подключен к серверу ");
+
+                    // Получение потока данных
+                    NetworkStream serverStream = client.GetStream();
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Не удалось подключиться к серверу");
+                }
+
+                Thread clientThread = new Thread(new ThreadStart(ReceiveMessages));
+                clientThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка: " + ex.Message);
+            }
+        }
+        private void ReceiveMessages()
+        {
+            NetworkStream serverStream = client.GetStream();
+            byte[] receiveBuffer = new byte[1024];
+
+            while (true)
+            {
+                try
+                {
+
+                    int bytesRead = serverStream.Read(receiveBuffer, 0, receiveBuffer.Length);
+                    string receivedMessage = Encoding.ASCII.GetString(receiveBuffer, 0, bytesRead);
+
+                    textBoxChat.Invoke((MethodInvoker)(() =>
+                    {
+                        textBoxChat.Text += "Вы: " + receivedMessage + Environment.NewLine;
+                    }));
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+
+            client.Close();
+        }
+        private void btnsend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!client.Connected)
+                {
+                    client.Connect("", 8888);
+                }
+
+                string message = textBoxMessage.Text;
+                byte[] sendBuffer = Encoding.UTF8.GetBytes(message);
+
+                NetworkStream serverStream = client.GetStream();
+                serverStream.Write(sendBuffer, 0, sendBuffer.Length);
+                serverStream.Flush();
+
+                textBoxChat.Text += "Вы: " + message + Environment.NewLine;
+                textBoxMessage.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка: " + ex.Message);
+            }
+        }
+
+        private void textBoxMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxChat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
